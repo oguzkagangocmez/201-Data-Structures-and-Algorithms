e6c2779e7473213783a4db095e40bbd6294a7cd6
oguzkagangocmez <gocmezoguzkagan@gmail.com>
2026-03-25 16:49:56 +0300

    added another test case for capitalCity() function

--- src/test/java/TestArrayGraph.java
+++ src/test/java/TestArrayGraph.java
@@ -18,6 +18,16 @@ public class TestArrayGraph {
                         {2, 0, INF}, {2, 1, 1}, {2, 3, 1},
                         {3, 0, INF}, {3, 1, INF}, {3, 2, 1}});
         assertEquals(1, graph.capitalCity());
+        graph = new Graph(5, new int[][]{
+            {0, 1, 10}, {1, 0, 10},
+            {0, 2, 2}, {2, 0, 2},
+            {0, 3, 2}, {3, 0, 2},
+            {1, 4, 1}, {4, 1, 1},
+            {2, 4, 1}, {4, 2, 1},
+            {3, 4, 8}, {4, 3, 8}
+    });
+
+        assertEquals(0, graph.capitalCity());
     }
 
     @Test
